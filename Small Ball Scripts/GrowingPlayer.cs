21df5cd2730e84d57409302445ac93cb89557fd8
Scottwallace1016 <33709121+Scottwallace1016@users.noreply.github.com>
2019-09-30 17:46:25 -0400

    Small Ball Scripts Uploaded
    GitHub wouldn't let me upload the entirety of the game so here is the logic of the game I have written.

---
+++ Small Ball Scripts/GrowingPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingPlayer : MonoBehaviour {
+    public Transform player;
+    public float sizeIncrease = 0.1f; 
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        player.localScale += new Vector3(sizeIncrease * Time.deltaTime, sizeIncrease * Time.deltaTime, sizeIncrease * Time.deltaTime);
+	}
+}
---
+++ Small Ball Scripts/PlayButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
+
+public class PlayButton : MonoBehaviour {
+    public static bool gameStarted = false;
+    
+
+	public void PlayGame()
+    {
+        if (!Advertisement.isShowing)
+        {
+            SceneManager.LoadScene(3);
+            gameStarted = true;
+        }
+
+        gameStarted = false;
+ 
+       
+        Trigger.counter = 0;
+       
+       
+   
+    }
+
+}
